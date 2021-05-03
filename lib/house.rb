54baf6a5cfd2cd9b35d727aa5f9d571653a56062
Kira Corbett <kiracorbett00@gmail.com>
2021-05-03 12:45:48 -0700

    Passed test 1

---
+++ lib/house.rb
@@ -0,0 +1,7 @@
+# Too much. :) Try starting from scratch, and do only the bare minimum to get the "line" tests to pass. Then get the "recite" method to pass by doing the most simple thing you can but leveraging your line method. The current implementation doesn't hit the marks for the three qualities of shameless green.
+
+class House
+  def line(num)
+    "This is the house that Jack built.\n"
+  end
+end
