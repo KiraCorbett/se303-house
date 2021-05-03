7a656db51adb3db8fc141a683cbdc4de9d64dca5
Kira Corbett <kiracorbett00@gmail.com>
2021-05-03 12:48:51 -0700

    Passed test 2

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,13 @@
 
 class House
   def line(num)
-    "This is the house that Jack built.\n"
+    case num
+    when 1
+      "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    else
+      "nil"
+    end
   end
 end
