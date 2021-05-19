dd7d445b87c9c3c9b57324a689bcc0c161977596
Kira Corbett <kiracorbett00@gmail.com>
2021-05-19 08:10:08 -0700

    Apply flocking rules to phrase 1

--- lib/house.rb
+++ lib/house.rb
@@ -1,53 +1,45 @@
+# Flocking Rules
+# 1. Select the things that are most alike.
+# 2. Find the smallest difference between them.
+# 3. Make the simplest change that will remove that difference.
+
 class House
   def line(num)
-    line_of_house = define_house
     case num
     when 1
-      line_of_house[0]
+      "This is #{phrase}"
     when 2
-      line_of_house[1]
+      "This is the malt that lay in the house that Jack built.\n"
     when 3
-      line_of_house[2]
+      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
-      line_of_house[3]
+      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 5
-      line_of_house[4]
+      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 6
-      line_of_house[5]
+      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 7
-      line_of_house[6]
+      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 8
-      line_of_house[7]
+      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 9
-      line_of_house[8]
+      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 10
-      line_of_house[9]
+      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 11
-      line_of_house[10]
+      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 12
-      line_of_house[11]
+      "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    else
+      "nil"
     end
   end
 
-  def recite()
-    lines = define_house
-    lines.join("\n")
+  def phrase
+    "the house that Jack built.\n"
   end
 
-  def define_house
-    house = []
-    house.push("This is the house that Jack built.\n")
-    house.push("This is the malt that lay in the house that Jack built.\n")
-    house.push("This is the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    house.push("This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n")
-    return house
+  def recite
+    (1..12).collect { |i| line(i) }.join("\n")
   end
 end
