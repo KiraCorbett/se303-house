0a712904741b1824e18013a7b6b2ad1d86037b7b
Kira Corbett <kiracorbett00@gmail.com>
2021-05-26 16:55:30 -0700

    WIP: line 10 to array

--- lib/house.rb
+++ lib/house.rb
@@ -31,7 +31,7 @@ class House
     elsif number == 9
       "#{line_array(number)}"
     elsif number == 10
-      "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+      "#{line_array(number)}"
     elsif number == 11
       "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     elsif number == 12
@@ -43,6 +43,7 @@ class House
 
   def line_array(num)
     verses = [
+      "the rooster that crowed in the morn that woke ",
       "the priest all shaven and shorn that married ",
       "the man all tattered and torn that kissed ",
       "the maiden all forlorn that milked ",
